0da5e9a1702d609d6409ed7859f7b5790136875e
Giancarlos Castillo <gc.c@outlook.com>
2023-02-10 19:34:47 -0400

    feat: the choosen pessimist

---
+++ src/content/el-pesimista-elegido.tsx
@@ -0,0 +1,49 @@
+---
+layout: post
+author: Gian
+title: 'El Pesimista Elegido'
+date: 2023-02-10 00:00:00
+tags: ['Abstracto']
+draft: false
+image: img/pessimist.jpg
+---
+
+El pesimista elegido,
+siempre ve la vida al revés,
+carga a sus espaldas un peso,
+que no lo deja ver más allá de sus pies.
+
+La tristeza lo envuelve,
+como una nube oscura,
+y aunque quiere encontrar la luz,
+su mente se torna más insegura.
+
+Es fácil juzgarlo por su actitud,
+y decir que es un derrotado,
+pero su visión del mundo es su virtud,
+y a veces más acertada que lo imaginado.
+
+La verdad es su estandarte,
+y la honestidad su bandera,
+él no se engaña a sí mismo,
+y lo que dice siempre lo piensa.
+
+Sus miedos son los de todos,
+pero los enfrenta con valentía,
+su corazón late sin descanso,
+y su alma siempre está en vigilia.
+
+Quizás en su pesimismo,
+hay una lucha constante,
+por encontrar la felicidad,
+aunque el camino sea desalentante.
+
+No es fácil ser un pesimista,
+ni cargar con tanto dolor,
+pero su fortaleza es su vista,
+y su corazón lleno de amor.
+
+Así que no juzguemos al elegido,
+que camina por el mundo al revés,
+tal vez no sepamos su sufrido,
+y el tesoro que lleva en su ser.
